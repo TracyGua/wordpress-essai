51d29ec53ecd4f142353c2eaa14c342edbab1960
TracyGua <90851849+TracyGua@users.noreply.github.com>
2022-11-11 13:28:38 -0500

    Update header.php

--- wp-content/themes/bootstrap-basic/header.php
+++ wp-content/themes/bootstrap-basic/header.php
@@ -73,7 +73,7 @@
 
                 <!-- Hamburger button -->
                 <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#mainNav" aria-controls="mainNav" aria-expanded="false" aria-label="Affichage/masquage de la navigation">
-                    <span class="navbar-toggler-icon"><img src="<?php echo get_template_directory_uri(). 'assets/icones/burger_icon.png'; ?>" alt=""></span>
+                    <span class="navbar-toggler-icon"><img src="<?php echo get_template_directory_uri(). '/assets/icones/burger_icon.png'; ?>" alt=""></span>
                 </button>
 
 							<div id="mainNav" class="collapse navbar-collapse">
